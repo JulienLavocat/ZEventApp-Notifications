82b40da7e43b8f0e70ee9a2f5f46872758161e44
Julien Lavocat <julien.lavocat@gmail.com>
2021-10-24 15:22:55 +0200

    More env var for elasticsearch

--- src/elasticsearch.ts
+++ src/elasticsearch.ts
@@ -5,6 +5,10 @@ const elasticIndex = "games_autocomplete";
 
 const elastic = new Client({
 	node: process.env.ELASTIC_HOST,
+	auth: {
+		username: process.env.ELASTIC_USERNAME || "",
+		password: process.env.ELASTIC_PASSWORD || "",
+	},
 });
 
 export const ensureIndex = () =>
